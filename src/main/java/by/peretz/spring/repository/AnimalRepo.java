f909922fa07f1b393bf7780fddea434526fbabc8
peretz90 <peretz90@mail.ru>
2020-08-17 18:37:52 +0300

    commit peretz90

--- src/main/java/by/peretz/spring/repository/AnimalRepo.java
+++ src/main/java/by/peretz/spring/repository/AnimalRepo.java
@@ -4,12 +4,12 @@ import by.peretz.spring.domain.Animal;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AnimalRepo extends JpaRepository<Animal, Long> {
+  @Override
   Page<Animal> findAll(Pageable pageable);
   Page<Animal> findByNameStartingWithIgnoreCaseAndSpeciesStartsWithIgnoreCase(
       @Param("name") String name,
